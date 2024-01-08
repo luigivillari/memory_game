25cf4ebde7ab5624e925c72cbd02e0fa06922bb5
gigi010 <luigivillari9@gmail.com>
2024-01-08 19:44:17 +0100

    update

---
+++ difficulty.js
@@ -0,0 +1,32 @@
+const backbutton = document.getElementById('back-button');
+backbutton.addEventListener('click', homePage);
+
+const easy=document.getElementById('easy');
+easy.addEventListener('click',homePageEasy);
+
+const normal=document.getElementById('normal');
+normal.addEventListener('click',homePageNormal);
+
+const hard=document.getElementById('hard');
+hard.addEventListener('click',homePageHard);
+
+
+
+function homePage(){
+    window.location.href='file:///Users/luigivillari/htdocs/memory_game/home.html';
+}
+
+function homePageEasy(){
+    const easyValue = document.getElementById('easy').innerText;
+    window.location.href='file:///Users/luigivillari/htdocs/memory_game/home.html?value='+ easyValue;
+}
+
+ function homePageNormal(){
+    const normalValue = document.getElementById('normal').innerText;
+    window.location.href='file:///Users/luigivillari/htdocs/memory_game/home.html?value='+ normalValue;
+ }
+
+function homePageHard(){
+    const hardValue = document.getElementById('hard').innerText;
+    window.location.href='file:///Users/luigivillari/htdocs/memory_game/home.html?value='+ hardValue;
+ }
